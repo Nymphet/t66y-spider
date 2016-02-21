ae000c072676ff2cb2733a180fd3697805b24cc4
Zhi Zi <lolicon@pku.edu.cn>
2016-02-21 23:14:50 +0800

    add a spider for Xin Shi Dai De Wo Men board

--- t66ySpider/t66ySpider/items.py
+++ t66ySpider/t66ySpider/items.py
@@ -6,3 +6,7 @@ import scrapy
 class T66YspiderDagaierItem(scrapy.Item):
     t_title         = scrapy.Field()
     t_image_list    = scrapy.Field()
+
+class T66YspiderXinshidaiItem(scrapy.Item):
+    t_title         = scrapy.Field()
+    t_image_list    = scrapy.Field()
---
+++ t66ySpider/t66ySpider/spiders/t66yXinshidaiSpider.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import scrapy
+
+from t66ySpider.items import T66YspiderXinshidaiItem
+
+
+class t66yDagaierSpider(scrapy.Spider):
+    name = 'XinShiDai'
+    allowed_domains = ['t66y.com']
+    start_urls = ["http://t66y.com/thread0806.php?fid=8&search=&page=1"]
+    unicode_next_page = u'\u4e0b\u4e00\u9801'
+
+    def parse(self, response):
+        thread_hrefs = response.selector.xpath('//h3/a/@href')
+
+        for thread_href in thread_hrefs:
+            thread_url = response.urljoin(thread_href.extract())
+            yield scrapy.Request(thread_url, callback=self.parse_thread)
+
+        next_page_href = response.selector.xpath(
+            "//a[text()='%s']/@href" % self.unicode_next_page)[0]
+        next_page_url = response.urljoin(next_page_href.extract())
+
+        yield scrapy.Request(next_page_url, callback=self.parse)
+
+    def parse_thread(self, response):
+        item = T66YspiderXinshidaiItem()
+        item['t_title'] = response.selector.xpath(
+            'string(//title)')[0].extract()
+        item['t_image_list'] = response.selector.xpath(
+            '//input/@src').extract()
+        yield item
