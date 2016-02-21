a4522cad58b68bf4dd5f67a1ebeb7f501d194744
Zhi Zi <lolicon@pku.edu.cn>
2016-02-21 22:38:45 +0800

    add a spider for Da Gai Er De Qi Zhi board

--- t66ySpider/t66ySpider/items.py
+++ t66ySpider/t66ySpider/items.py
@@ -1,14 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
-class T66YspiderItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class T66YspiderDagaierItem(scrapy.Item):
+    t_title         = scrapy.Field()
+    t_image_list    = scrapy.Field()
